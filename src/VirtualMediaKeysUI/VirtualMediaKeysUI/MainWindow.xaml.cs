da995ef6c0ee434beb627ddaca4c992dc12dd16f
Paul <pvdstel@users.noreply.github.com>
2018-09-23 03:35:53 -0400

    Add handlers

--- src/VirtualMediaKeysUI/VirtualMediaKeysUI/MainWindow.xaml.cs
+++ src/VirtualMediaKeysUI/VirtualMediaKeysUI/MainWindow.xaml.cs
@@ -84,5 +84,35 @@ namespace VirtualMediaKeysUI
         {
             Close();
         }
+
+        private void Previous_Click(object sender, RoutedEventArgs e)
+        {
+            Input.SendKeyPress(Input.KeyCode.MEDIA_PREV_TRACK);
+        }
+
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+            Input.SendKeyPress(Input.KeyCode.MEDIA_PLAY_PAUSE);
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            Input.SendKeyPress(Input.KeyCode.MEDIA_NEXT_TRACK);
+        }
+
+        private void VolumeDown_Click(object sender, RoutedEventArgs e)
+        {
+            Input.SendKeyPress(Input.KeyCode.VOLUME_DOWN);
+        }
+
+        private void VolumeMute_Click(object sender, RoutedEventArgs e)
+        {
+            Input.SendKeyPress(Input.KeyCode.VOLUME_MUTE);
+        }
+
+        private void VolumeUp_Click(object sender, RoutedEventArgs e)
+        {
+            Input.SendKeyPress(Input.KeyCode.VOLUME_UP);
+        }
     }
 }
