6b83b70779622ffb0da9ae92c6ce4c51e19cef57
Paul <pvdstel@users.noreply.github.com>
2018-09-23 03:38:51 -0400

    Update assembly info

--- src/VirtualMediaKeysUI/VirtualMediaKeysUI/Properties/AssemblyInfo.cs
+++ src/VirtualMediaKeysUI/VirtualMediaKeysUI/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VirtualMediaKeysUI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Virtual Media Keys")]
+[assembly: AssemblyDescription("UI for onscreen virtual media keys.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VirtualMediaKeysUI")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("pvdstel")]
+[assembly: AssemblyProduct("Virtual Media Keys UI")]
+[assembly: AssemblyCopyright("Copyright © pvdstel 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
