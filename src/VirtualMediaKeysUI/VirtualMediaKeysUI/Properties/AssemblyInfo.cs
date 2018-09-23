9b795a21b0127b0f55a80e03ec5899408e9685b5
Paul <pvdstel@users.noreply.github.com>
2018-09-23 03:53:17 -0400

    Change product title

--- src/VirtualMediaKeysUI/VirtualMediaKeysUI/Properties/AssemblyInfo.cs
+++ src/VirtualMediaKeysUI/VirtualMediaKeysUI/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Virtual Media Keys")]
+[assembly: AssemblyTitle("Virtual Media Keys UI")]
 [assembly: AssemblyDescription("UI for onscreen virtual media keys.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("pvdstel")]
